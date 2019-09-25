f5467c5bb017a728c3f6f083e52cd3a83fd3b62a
rjhans7 <roosevelt.ubaldo@outlook.com>
2019-09-25 13:04:48 -0500

    uml

--- src/command/GarageDoor.java
+++ src/command/GarageDoor.java
@@ -1,6 +1,5 @@
 package command;
 
-<<<<<<< HEAD
 public class GarageDoor{
     private boolean isOn = false;
     GarageDoor(String place){}
@@ -17,17 +16,3 @@ public class GarageDoor{
     }
 
 }
-=======
-public class GarageDoor {
-    private boolean isOpen = false;
-    public void open(){
-        isOpen =true;
-        System.out.println("Door is Open");
-    }
-    public void close(){
-        isOpen =false;
-        System.out.println("Door is Close");
-    }
-
-}
->>>>>>> 2cb91900c03633838c34c9b9a96ef1316dbabf3b
